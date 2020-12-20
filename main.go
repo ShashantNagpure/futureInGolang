0a9a54647d907e9e01803342299ab08a9f33712b
unknown <nagpureshashant@gmail.com>
2020-12-20 19:36:45 +0530

    adding callback function

--- main.go
+++ main.go
@@ -18,12 +18,13 @@ type Future interface {
 
 // FutureTask Future object
 type FutureTask struct {
-	success bool
-	error   error
-	running bool
-	done    bool
-	result  Result
-	channel <-chan Result
+	success        bool
+	error          error
+	running        bool
+	done           bool
+	result         Result
+	channel        <-chan Result
+	callbackMethod func()
 }
 
 // Result holds the result of callable
@@ -36,6 +37,9 @@ func (futureTask *FutureTask) get() Result {
 	if futureTask.done {
 		return futureTask.result
 	}
+	if futureTask.callbackMethod != nil {
+		defer futureTask.callbackMethod()
+	}
 	ctx := context.Background()
 	return futureTask.getWithContext(ctx)
 }
@@ -44,12 +48,16 @@ func (futureTask *FutureTask) getWithTimeout(timeout time.Duration) Result {
 	if futureTask.done {
 		return futureTask.result
 	}
+	if futureTask.callbackMethod != nil {
+		defer futureTask.callbackMethod()
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	return futureTask.getWithContext(ctx)
 }
 
 func (futureTask *FutureTask) getWithContext(ctx context.Context) Result {
+	fmt.Println("Executing getContext to receive from channel")
 	select {
 	case <-ctx.Done():
 		futureTask.done = true
@@ -92,6 +100,9 @@ func (futureTask *FutureTask) cancel() bool {
 	if futureTask.complete() || futureTask.isCancelled() || futureTask.isRunning() {
 		return false
 	}
+	if futureTask.callbackMethod != nil {
+		defer futureTask.callbackMethod()
+	}
 	interruptionError := &InterruptError{errorString: "Cancelled Manually"}
 	futureTask.done = true
 	futureTask.success = false
@@ -106,12 +117,17 @@ func (futureTask *FutureTask) isRunning() bool {
 	}
 	return false
 }
+
+//Stringer method for result
 func (result Result) String() string {
 	err := "no"
 	if result.error != nil {
 		err = result.error.Error()
 	}
 	return fmt.Sprintf("%v with (%s error)", result.value, err)
+}
+func (futureTask *FutureTask) addDoneCallback(callbackMethod func()) {
+	futureTask.callbackMethod = callbackMethod
 }
 
 //ReturnAFuture creates a new future for task func
@@ -163,6 +179,7 @@ func main() {
 	})
 	f2 := futureInstance2.get()
 	fmt.Println(f2)
+	fmt.Println("------------")
 
 	//example of timeout error
 	futureInstance1 := ReturnAFuture(func() Result {
@@ -173,6 +190,7 @@ func main() {
 	})
 	f1 := futureInstance1.getWithTimeout(1 * time.Second)
 	fmt.Println(f1)
+	fmt.Println("------------")
 
 	//example of cancel operation
 	futureInstance3 := ReturnAFuture(func() Result {
@@ -185,5 +203,20 @@ func main() {
 	fmt.Println("Cancel operation:", ok)
 	f3 := futureInstance3.get()
 	fmt.Println(f3)
+	fmt.Println("------------")
+
+	//example of callback
+	futureInstance4 := ReturnAFuture(func() Result {
+		var res interface{}
+		res = "50"
+		time.Sleep(2 * time.Second)
+		return Result{value: res}
+	})
+	futureInstance4.addDoneCallback(func() {
+		fmt.Println("Executing callback function")
+	})
+	f4 := futureInstance4.get()
+	fmt.Println(f4)
+	fmt.Println("------------")
 
 }
